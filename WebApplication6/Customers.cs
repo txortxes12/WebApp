65fb6870103f945e9fa2162d4f76137150b7bc99
txortxes12 <76061254+txortxes12@users.noreply.github.com>
2020-12-15 18:40:43 +0100

    Update Customers.cs

--- WebApplication6/Customers.cs
+++ WebApplication6/Customers.cs
@@ -22,7 +22,7 @@ namespace WebApplication6
         public string City { get; set; }
         public string Region { get; set; }
         public string PostalCode { get; set; }
-        public string Country { get; set; }
+        public string Country { get; set; } //
         public string Phone { get; set; } 
         public string Fax { get; set; } 
     }
