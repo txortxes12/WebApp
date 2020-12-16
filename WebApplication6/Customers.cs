6c73197b2dc6ea3526a113c901fa8a0d110c2201
jalca <jalca@DESKTOP-FA4UK8P>
2020-12-16 12:21:29 +0100

    sin comentarios

--- WebApplication6/Customers.cs
+++ WebApplication6/Customers.cs
@@ -21,7 +21,7 @@ namespace WebApplication6
         public string Address { get; set; } 
         public string City { get; set; }
         public string Region { get; set; }
-        public string PostalCode { get; set;// }
+        public string PostalCode { get; set; }
         public string Country { get; set; } 
         public string Phone { get; set; } 
         public string Fax { get; set; } 
